936ccfdb411bb40bb7ded2969512fa730a895d51
Britt Karlström <britt.karlstrom@student.howest.be>
2020-01-06 20:27:15 +0100

    Add Unit Test for MainViewModel

---
+++ B4.EE.KarlstromB.Tests/MainViewModelTests.cs
@@ -0,0 +1,61 @@
+﻿using B4.EE.KarlstromB.Domain.Models;
+using B4.EE.KarlstromB.Domain.Services;
+using B4.EE.KarlstromB.ViewModels;
+using FreshMvvm;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace B4.EE.KarlstromB.Tests
+{
+    public class MainViewModelTests
+    {
+        private Mock<IAppSettingsService> appSettingsMock;
+        private Mock<IUsersService> usersServiceMock;
+        private Mock<IPageModelCoreMethods> coreMethodsMock;
+
+        public MainViewModelTests()
+        {
+            appSettingsMock = new Mock<IAppSettingsService>();
+            usersServiceMock = new Mock<IUsersService>();
+            coreMethodsMock = new Mock<IPageModelCoreMethods>();
+        }
+
+        private MainViewModel GetMainViewModel()
+        {
+            var vm = new MainViewModel(usersServiceMock.Object, appSettingsMock.Object)
+            {
+                CoreMethods = coreMethodsMock.Object
+            };
+            return vm;
+        }
+
+        [Fact]
+        public void OpenOverviewPageCommand_Opens_OverviewPage()
+        {
+            //arrange
+            var vm = GetMainViewModel();
+
+            //act
+            vm.OpenOverviewPageCommand.Execute(null);
+
+            //assert
+            coreMethodsMock.Verify(e => e.PushPageModel<OverviewViewModel>(It.IsAny<bool>()), Times.Once());
+        }
+
+        [Fact]
+        public void OpenSettingsPageCommand_Opens_SettingsPage()
+        {
+            //arrange
+            var vm = GetMainViewModel();
+
+            //act
+            vm.OpenSettingsPageCommand.Execute(null);
+
+            //assert
+            coreMethodsMock.Verify(e => e.PushPageModel<SettingsViewModel>(It.IsAny<bool>()), Times.Once());
+        }
+    }
+}
